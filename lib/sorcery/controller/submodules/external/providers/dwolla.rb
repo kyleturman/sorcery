e630933776394bed54952cb9de1fb42392a4dca2
Kyle Turman <kyle@kyleis.com>
2012-12-23 21:40:45 -0600

    OAuth Progress

--- lib/sorcery/controller/submodules/external/providers/dwolla.rb
+++ lib/sorcery/controller/submodules/external/providers/dwolla.rb
@@ -44,10 +44,10 @@ module Sorcery
             
                 def init
                   @site           = "https://www.dwolla.com"
-                  @scope          = "accountinfofull"
+                  @scope          = nil
                   @auth_path      = "/oauth/v2/authenticate"
                   @token_path     = "/oauth/v2/token"
-                  @user_info_url  = "/oauth/rest/users"
+                  @user_info_url  = "https://www.dwolla.com/oauth/rest/users/"
                   @user_info_mapping = {}
                 end
                 
@@ -63,20 +63,10 @@ module Sorcery
                   true
                 end
                 
-                def user
-                	@user ||= ::Dwolla::User.me(access_token.token).fetch
-                end
-                
                 # calculates and returns the url to which the user should be redirected,
                 # to get authenticated at the external provider's site.
                 def login_url(params,session)
                   self.authorize_url({:authorize_url => @auth_path})
-                end
-
-                # overrides oauth2#authorize_url to allow customized scope.
-                def authorize_url
-                  @scope = self.access_permissions.present? ? self.access_permissions.join(",") : @scope
-                  super
                 end
 
                 # tries to login the user from access token
@@ -84,7 +74,7 @@ module Sorcery
                   args = {}
                   args.merge!({:code => params[:code]}) if params[:code]
                   options = {
-                    :token_url => @token_url,
+                    :token_url => @token_path,
                     :token_method => :post
                   }
                   @access_token = self.get_access_token(args, options)
