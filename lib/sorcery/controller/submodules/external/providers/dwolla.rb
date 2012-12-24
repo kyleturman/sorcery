28ae86ade9019b307f719c8b1382d19ed23abce7
Kyle Turman <kyle@kyleis.com>
2012-12-24 00:39:44 -0600

    Trying to force the param_name.

--- lib/sorcery/controller/submodules/external/providers/dwolla.rb
+++ lib/sorcery/controller/submodules/external/providers/dwolla.rb
@@ -56,6 +56,8 @@ module Sorcery
                 
                 def get_user_hash
                   user_hash = {}
+                  put "\n\nACCESS TOKEN: #{@access_token.inspect}\n\n"
+                  @access_token.param_name = @param_name
                   response = @access_token.get(@user_info_url)
                   user_hash[:user_info] = JSON.parse(response.body)
                   user_hash[:uid] = user_hash[:user_info]['id']
